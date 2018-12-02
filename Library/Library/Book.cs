9bb3686564ed46fcc6bcdb696cc50da7c4e26148
Michael Kelly <L00137447@student.lyit.ie>
2018-12-02 21:23:40 +0000

    Initial commit

---
+++ Library/Library/Book.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    class Book
+    {
+        private string title;
+        private string author;
+        private int pages;
+        private int wordCount;
+
+        public Book(string title)
+        {
+            this.title = title;
+            
+        }
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+            
+        }
+        public Book(string title, string author, int pages, int wordCount)
+        {
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.wordCount = wordCount;
+        }
+        public string GetTitle()
+        {
+            return title;
+        }
+        public void SetTitle(string title)
+        {
+            this.title = title;
+        }
+        public void AssignWordCountFrom (string text)
+        {
+            wordCount = text.Split(' ').Length;
+        }
+    }
+}
